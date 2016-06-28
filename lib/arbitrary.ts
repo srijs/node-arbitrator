34577a43a579b3bb3a5bc445fcd7bdc5275d70f4
Sam Rijs <srijs@airpost.net>
2016-06-28 19:15:46 +1000

    add random generator and gen/arbitrary

---
+++ lib/arbitrary.ts
@@ -0,0 +1,42 @@
+import {Tree} from './tree';
+import {Gen} from './gen';
+
+export interface IArbitrary<A> {
+  generator: Gen<Tree<A>>;
+}
+
+export class Arbitrary<A> {
+  constructor(private _: IArbitrary<A>) {}
+
+  static of<A>(a: A): Arbitrary<A> {
+    return new Arbitrary({
+      generator: Gen.of(Tree.of(a))
+    });
+  }
+
+  map<B>(f: (a: A) => B): Arbitrary<B> {
+    return new Arbitrary({
+      generator: this._.generator.map(tree => tree.map(f))
+    });
+  }
+
+  chain<B>(f: (a: A) => Arbitrary<B>): Arbitrary<B> {
+    return new Arbitrary({
+      generator: this._.generator
+        .chain(tree => Gen.traverseTree(a => f(a)._.generator, tree))
+        .map(Tree.flatten)
+    });
+  }
+
+  static fromGenWithShrink<A>(gen: Gen<A>, shrink: (a: A) => IterableIterator<A>): Arbitrary<A> {
+    return new Arbitrary({
+      generator: gen.map(a => Tree.unfoldTree(x => x, shrink, a))
+    });
+  }
+
+  static fromGen<A>(gen: Gen<A>): Arbitrary<A> {
+    return new Arbitrary({
+      generator: gen.map(Tree.of)
+    });
+  }
+}
---
+++ lib/gen.ts
@@ -0,0 +1,85 @@
+import {Chance} from 'chance';
+
+import {Tree} from './tree';
+
+export interface IGen<A> {
+  runGen(chance: Chance.Chance, size: number): A;
+}
+
+export class Gen<A> {
+  constructor(private _: IGen<A>) {}
+
+  static of<A>(a: A): Gen<A> {
+    return new Gen({runGen: () => a});
+  }
+
+  map<B>(f: (a: A) => B): Gen<B> {
+    return new Gen({
+      runGen: (chance, size) => f(this._.runGen(chance, size))
+    });
+  }
+
+  chain<B>(f: (a: A) => Gen<B>): Gen<B> {
+    return new Gen({
+      runGen: (chance, size) => f(this._.runGen(chance, size))._.runGen(chance, size)
+    });
+  }
+
+  /**
+   * Used to construct generators that depend on the size parameter.
+   */
+  static sized<A>(f: (size: number) => Gen<A>): Gen<A> {
+    return new Gen({
+      runGen: (chance, size) => f(size)._.runGen(chance, size)
+    });
+  }
+
+  /**
+   * Overrides the size parameter. Returns a generator which uses
+   * the given size instead of the runtime-size parameter.
+   */
+  resize(size: number): Gen<A> {
+    return new Gen({
+      runGen: (chance) => this._.runGen(chance, size)
+    });
+  }
+
+  /**
+   * Adjust the size parameter, by transforming it with the given
+   * function.
+   */
+  scale(f: (size: number) => number): Gen<A> {
+    return Gen.sized(size => this.resize(size));
+  }
+
+  /**
+   * Run a generator. The size passed to the generator is always 30;
+   * if you want another size then you should explicitly use #resize.
+   */
+  generate(): A {
+    return this._.runGen(new Chance(), 30);
+  }
+
+  private static _traverseForest<A, B>(f: (a: A) => Gen<B>, forest: () => IterableIterator<Tree<A>>): Gen<() => IterableIterator<Tree<B>>> {
+    return new Gen({
+      runGen: (chance, size) => {
+        return function*() {
+          for (let tree of forest()) {
+            yield Gen.traverseTree(f, tree)._.runGen(chance, size);
+          }
+        };
+      }
+    });
+  }
+
+  static traverseTree<A, B>(f: (a: A) => Gen<B>, tree: Tree<A>): Gen<Tree<B>> {
+    return f(tree.outcome).chain(b => {
+      return Gen._traverseForest(f, () => tree.shrink()).map(shrink => {
+        return new Tree({
+          outcome: b,
+          shrink: shrink
+        });
+      });
+    });
+  }
+}
